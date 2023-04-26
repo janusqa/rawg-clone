283e6f499b9aca4689e0cd9b26c3b4de06403c57
Janus Developer <dev@retrievo.net>
2023-04-26 13:02:08 -0400

    add tanstack react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App.tsx';
 import './index.css';
 import theme from './theme';
 
+const queryClient = new QueryClient({
+    // we can override default options here or per query
+    // one of the popular ones to override per query is staleTime
+    defaultOptions: {
+        queries: {
+            retry: 3,
+            cacheTime: 1000 * 60 * 5, // default 5 mins
+            staleTime: 1000 * 10, // 10 seconds
+            refetchOnWindowFocus: true, //  default true
+            refetchOnReconnect: true, // default true
+            refetchOnMount: true, // default true
+        },
+    },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-            <App />
-        </ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <ColorModeScript
+                    initialColorMode={theme.config.initialColorMode}
+                />
+                <App />
+            </ChakraProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </React.StrictMode>
 );
