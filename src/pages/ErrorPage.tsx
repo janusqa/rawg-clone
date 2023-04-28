4b77b4e88d30a075e882960f09b3884b0b0e6581
Janus Developer <dev@retrievo.net>
2023-04-28 19:35:53 -0400

    add error boundary for root route

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,23 +1,20 @@
+import { Heading, Text, Box } from '@chakra-ui/react';
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
-    // Application errors are captured by router and we can capture and log them here
     const error = useRouteError();
 
-    // Once we have the error we can determine if it is an application error or a route error
-    //>> isRouteErrorResponse(error);
+    const errorMessage = isRouteErrorResponse(error)
+        ? 'This page does not exist'
+        : error instanceof Error
+        ? error.message
+        : 'Sorry, an unexpected error has occurred';
 
     return (
-        <>
-            <h1>Oops...</h1>
-            <p>
-                {isRouteErrorResponse(error)
-                    ? 'Page not found'
-                    : error instanceof Error
-                    ? error.message
-                    : 'Sorry, an unexpected error has occurred'}
-            </p>
-        </>
+        <Box padding={5}>
+            <Heading>Oops...</Heading>
+            <Text>{errorMessage}</Text>
+        </Box>
     );
 };
 
